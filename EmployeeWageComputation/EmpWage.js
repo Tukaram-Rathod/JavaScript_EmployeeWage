d5204c1c69b8297d549fa0c4ecb7b2ad635345d4
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-05-17 18:23:51 +0530

    Added.Calculate Wage total working hours of 160 or 20 days for  months

--- EmployeeWageComputation/EmpWage.js
+++ EmployeeWageComputation/EmpWage.js
@@ -1,15 +1,22 @@
-//uc-4
+//uc-5
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOUR = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 2;
-let empHrs = 0; empHrs = 4
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+
+const NUM_OF_WORKING_DAYS = 10;
+const MAX_HRS_IN_MONTH = 100;
+
+let empHrs = 0;  
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+//while loop
+while ( totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);
-}
+    totalEmpHrs += getWorkingHours(empCheck);
+}   
 //function 
 function getWorkingHours(empCheck){
     switch (empCheck){
@@ -22,8 +29,8 @@ function getWorkingHours(empCheck){
     }
 }
 //Computation
-let empWage = empHrs * WAGE_PER_HOUR;
-empHrs = getWorkingHours(empWage);
-console.log(" Total Hrs: " + empHrs + " Emp Wage: " + empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+// empHrs = getWorkingHours(empWage);
+console.log(" Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
 
 
