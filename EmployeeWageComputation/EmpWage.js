55bffc3135dff89aa71e6b775f50a4095eadc1cc
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-05-17 17:33:09 +0530

    Added.Refactored code to write Function gor daily working Hours

--- EmployeeWageComputation/EmpWage.js
+++ EmployeeWageComputation/EmpWage.js
@@ -1,4 +1,4 @@
-//uc-2
+//uc-3
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOUR = 4;
@@ -6,19 +6,21 @@ const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
 
 let empHrs = 0; empHrs = 4 
-empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck){
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOUR;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOUR;
-        break;
-    default:
-        empHrs = 0;
-
+let empCheck = Math.floor(Math.random() * 10) % 3;
+//function 
+function getWorkingHours(empCheck){
+    switch (empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOUR;            
+        case IS_FULL_TIME:
+            return FULL_TIME_HOUR;            
+        default:
+            empHrs = 0;
+    }
 }
+//Computation
 let empWage = empHrs * WAGE_PER_HOUR;
+empHrs = getWorkingHours(empWage);
 console.log("Emp Wage: " + empWage);
 
 
